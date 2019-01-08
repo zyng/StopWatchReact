2f8d0edbe2ed1b03b9e768e124bf318350660d43
zyng9 <kisielewskimichal@gmail.com>
2019-01-08 21:22:35 +0100

    Updates

---
+++ precache-manifest.0118cc054d0f42f200e3cdc33e759c43.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec2a1637fa69546a67530729b49282ce",
+    "url": "/StopWatchReact/static/media/Flaticon.ec2a1637.eot"
+  },
+  {
+    "revision": "c25bddf721d4bb83864b83a53248e5ad",
+    "url": "/StopWatchReact/static/media/Flaticon.c25bddf7.woff"
+  },
+  {
+    "revision": "8bc4ad0692b9f47957b5e4eafac309da",
+    "url": "/StopWatchReact/static/media/Flaticon.8bc4ad06.ttf"
+  },
+  {
+    "revision": "4893016506548c75a50e6eca57eec0ad",
+    "url": "/StopWatchReact/static/media/Flaticon.48930165.svg"
+  },
+  {
+    "revision": "11ac9692f2fe2f384b4a",
+    "url": "/StopWatchReact/static/js/runtime~main.11ac9692.js"
+  },
+  {
+    "revision": "13434aea52124a2f7f5b",
+    "url": "/StopWatchReact/static/js/main.13434aea.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/StopWatchReact/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "13434aea52124a2f7f5b",
+    "url": "/StopWatchReact/static/css/main.a6589d51.chunk.css"
+  },
+  {
+    "revision": "db3c47ada3a4741e4d55395f7ba75fb1",
+    "url": "/StopWatchReact/index.html"
+  }
+];
